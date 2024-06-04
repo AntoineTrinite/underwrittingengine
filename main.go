ea09b50be930ce00b9928bea0b59a62d0dc7867c
Trinité Antoine <antoine.trinite@gmail.com>
2024-06-04 14:24:28 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+
+	router := chi.NewMux()
+	router.Get("/test", HandleTest)
+
+	http.ListenAndServe(":3000", router)
+}
+
+func HandleTest(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello test"))
+}
